bd1780803f7c7eb60e8c9822b147088a77aa9002
nico <nicolas.alv3@gmail.com>
2020-12-14 15:13:47 -0300

    First commit

---
+++ src/test/kotlin/com/mirtapp/mirtapp/MirtappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mirtapp.mirtapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MirtappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
